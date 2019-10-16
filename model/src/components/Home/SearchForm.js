200444dc6bc88c75a91444cca8ca8b135ec85a3f
HematielaBequille <clement.universe@gmail.com>
2019-10-16 10:23:49 +0200

    recherche avancée en cours

--- model/src/components/Home/SearchForm.js
+++ model/src/components/Home/SearchForm.js
@@ -6,10 +6,36 @@ import './home.scss';
 
 // == Composant
 const SearchForm = () => (
-    <div className="searchBar">
-        <input type="search" className="searchBar-input" placeholder="rechercher un évènement" />
-        <button className="searchBar-button">Rechercher</button>
-    </div>
+    <form>
+        <div className="searchBar">
+            <input type="search" className="searchBar-input" placeholder="Que recherchez-vous ?" />
+        </div>
+        <div className="searchBarAdvanced">
+            <label id="searchBarAdvanced-label_tag">Tag
+                <select name="tag">
+                    <option value="">Tag 1</option>
+                    <option value="">Tag 2</option>
+                    <option value="">Tag 3</option>
+                </select>
+            </label>
+            <label id="searchBarAdvanced-label_price">Prix
+                <input type="radio" className="searchBarAdvanced-price" name="prix" value="gratuit" />
+                <label for="gratuit">Gratuit</label>
+                <input type="radio" className="searchBarAdvanced-price" name="prix" value="payant" />
+                <label for="payant">Payant</label>
+            </label>
+            <label id="searchBarAdvanced-label_broadcast">Diffusion internet
+                <input type="radio" className="searchBarAdvanced-broadcast" name="Diffusion" value="oui" />
+                <label for="oui">Oui</label>
+                <input type="radio" className="searchBarAdvanced-broadcast" name="Diffusion" value="non" />
+                <label for="non">Non</label>
+            </label>
+            <label id="searchBarAdvanced-label_date">Date
+                <input type="date" name="date" min="2019-10-01" max="2020-02-29" />
+            </label>
+        </div>
+        <button type="submit" className="searchBar-button">Rechercher</button>
+    </form>
 );
 
 // == Export
