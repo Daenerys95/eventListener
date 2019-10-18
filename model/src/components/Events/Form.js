32b0f1edeead02563df3dca8bc33d9b947a2a5d1
HematielaBequille <clement.universe@gmail.com>
2019-10-18 11:11:56 +0200

    stop suite au point de 11h

--- model/src/components/Events/Form.js
+++ model/src/components/Events/Form.js
@@ -16,29 +16,29 @@ const Form = () => (
             <div className="searchBarAdvanced">
                 <p>Recherche avancée</p>
                 <input type="search" className="searchBar-input" placeholder="Tapez un mot-clef" />
-                <label id="searchBarAdvanced-label_tag">Tag
+                <label className="searchBarAdvanced-label_tag">Tag
                     <select name="tag">
                         <option value="">Tag 1</option>
                         <option value="">Tag 2</option>
                         <option value="">Tag 3</option>
                     </select>
                 </label>
-                <label id="searchBarAdvanced-label_price">Prix
+                <label clasName="searchBarAdvanced-label_price">Prix
                     <input type="radio" className="searchBarAdvanced-price" name="prix" value="gratuit" />
                     <label for="gratuit">Gratuit</label>
                     <input type="radio" className="searchBarAdvanced-price" name="prix" value="payant" />
                     <label for="payant">Payant</label>
                 </label>
-                <label id="searchBarAdvanced-label_broadcast">Diffusion internet
+                <label className="searchBarAdvanced-label_broadcast">Diffusion internet
                     <input type="radio" className="searchBarAdvanced-broadcast" name="diffusion" value="oui" />
                     <label for="oui">Oui</label>
                     <input type="radio" className="searchBarAdvanced-broadcast" name="diffusion" value="non" />
                     <label for="non">Non</label>
                 </label>
-                <label id="searchBarAdvanced-label_date">Date de début
+                <label className="searchBarAdvanced-label_date">Date de début
                     <input type="date" name="date" min="2019-10-01" max="2020-02-29" />
                 </label>
-                <label id="searchBarAdvanced-label_adress">Ville
+                <label className="searchBarAdvanced-label_adress">Ville
                     <input list="villes" id="searchBarAdvanced-label_adress-cities" name="ville" />
                     <datalist id="villes">
                         <option value="Ville 1" />
