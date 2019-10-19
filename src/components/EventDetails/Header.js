f10e83e0aeb4ce1e2f158e5a801984e58b9c1a7a
Ines Mouandjo Lobe <inesmouandjolobe@gmail.com>
2019-10-19 18:33:40 +0200

    click coeur ok, clic partage en cours

--- src/components/EventDetails/Header.js
+++ src/components/EventDetails/Header.js
@@ -1,5 +1,20 @@
 import React from 'react';
 
+import ShareLinks from 'src/components/Modal/ShareLinks';
+
+const handleClickHeart = (event) => {
+  if (event.target.src === 'https://img.icons8.com/nolan/24/000000/like.png') {
+    event.target.src = 'https://img.icons8.com/emoji/24/000000/red-heart.png';
+  }
+  else {
+    event.target.src = 'https://img.icons8.com/nolan/24/000000/like.png';
+  }
+};
+
+const handleClickShare = () => {
+
+};
+
 const Header = () => (
   <div className="header-event">
     <div className="header-event-bg" />
@@ -7,7 +22,7 @@ const Header = () => (
       <time className="header-event-infos-date">NOV.<br />12</time>
       <br />
       <p className="header-event-infos-name">
-            DataJob Paris 2019
+          DataJob Paris 2019
         <br />
         <span>par DataJob</span>
         <button type="button">Suivre</button>
@@ -17,8 +32,8 @@ const Header = () => (
     </div>
     <div className="header-event-bottom">
       <div className="header-event-bottom-icons">
-        <img alt="share" src="https://img.icons8.com/nolan/24/000000/share.png" />
-        <img alt="like" src="https://img.icons8.com/nolan/24/000000/like.png" />
+        <img onClick={handleClickShare} alt="share" src="https://img.icons8.com/nolan/24/000000/share.png" />
+        <img onClick={handleClickHeart} alt="like" src="https://img.icons8.com/nolan/24/000000/like.png" />
       </div>
       <div className="header-event-bottom-status">
         <p>A venir</p>
---
+++ src/components/Modal/ShareLinks.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ShareLinks = () => (
+  <div>
+    <img alt="fb-icon" src="https://img.icons8.com/nolan/64/000000/facebook-new.png" />
+    <img alt="fb-mes-icon" src="https://img.icons8.com/nolan/64/000000/facebook-messenger.png" />
+    <img alt="twi-icon" src="https://img.icons8.com/nolan/64/000000/twitter.png" />
+    <img alt="mail-icon" src="https://img.icons8.com/nolan/64/000000/important-mail.png" />
+  </div>
+);
+
+export default ShareLinks;
