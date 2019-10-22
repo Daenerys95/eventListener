7f93395e6fd1263b75f80048747e5a9145e38630
HematielaBequille <clement.universe@gmail.com>
2019-10-22 11:25:04 +0200

    transformation en stateful component

--- model/src/components/Events/Form.js
+++ model/src/components/Events/Form.js
@@ -5,80 +5,90 @@ import React from 'react';
 import './events.scss';
 
 // == Composant
-const Form = ({
-    address
-}) => (
-    console.log(address),
-    <div className="search">
-        <form>
-            <div className="searchBar">
-                <p>Recherche simple</p>
-                <input type="search" className="searchBar-input" placeholder="Que recherchez-vous ?" />
-                <button type="submit" className="searchBar-button" onClick={ButtonClick}>Rechercher</button>
-            </div>
-            <div className="searchBarAdvanced">
-                <p>Recherche avancée</p>
-                <input type="search" className="searchBar-input" placeholder="Tapez un mot-clef" />
-                <label className="searchBarAdvanced-label_tag">Tag
-                    <select name="tag" value={this.state.value}>
-                        <option value="{name.tag}">{name}</option>
-                        <option value="{name.tag}">{name}</option>
-                        <option value="{name.tag}">{name}</option>
-                    </select>
-                </label>
-                <label clasName="searchBarAdvanced-label_price">Prix
-                    <input type="radio" className="searchBarAdvanced-price" name="prix" value="gratuit" />
-                    <label for="gratuit">Gratuit</label>
-                    <input type="radio" className="searchBarAdvanced-price" name="prix" value="payant" />
-                    <label for="payant">Payant</label>
-                </label>
-                <fieldset className="searchBarAdvanced-label_broadcast">Diffusion internet
-                    <input type="radio" className="searchBarAdvanced-broadcast" name="diffusion" value="oui" />
-                    <label for="oui">Oui</label>
-                    <input type="radio" className="searchBarAdvanced-broadcast" name="diffusion" value="non" />
-                    <label for="non">Non</label>
-                </fieldset>
-                <label className="searchBarAdvanced-label_date">Date de début
-                    <input type="date" name="date" min="2019-10-01" max="2020-02-29" />
-                </label>
-                <label className="searchBarAdvanced-label_adress">Ville
-                    <input list="villes" id="searchBarAdvanced-label_adress-cities" name="ville" />
-                    <datalist id="villes">
-                        <option value={address} />
-                        <option value={address} />
-                        <option value={address} />
-                    </datalist>
-                </label>
-                <button type="submit" className="searchBar-button" onClick={ButtonClickAdvanced}>Rechercher</button>
-            </div>
-            
-        </form>
-    </div>
-);
-
-function ButtonClick(e) {
-    e.preventDefault();
+buttonClick() {
     console.log('bouton cliqué');
+    this.setState({
+        clicked : !this.state.clicked
+    });
     // effectuer ici une recherche par mot-clef
 }
 
-function ButtonClickAdvanced(e) {
-    e.preventDefault();
-    console.log('bouton cliqué');
-    // effectuer ici la recherche avancée
-}
+//function ButtonClickAdvanced(e) {
+//    e.preventDefault();
+//    console.log('bouton 2 cliqué ');
+//    // effectuer ici la recherche avancée
+//}
 
-function handleChange(event) {
-    this.setState({value: event.target.value});
-}
+//function handleChange(event) {
+//    this.setState({value: event.target.value});
+//}
 
-function handleSubmit(event) {
-    console.log('this.state.value');
-    event.preventDefault();
-}
+//function handleSubmit(event) {
+//    console.log('this.state.value');
+//    event.preventDefault();
+//}
 
 // https://fr.reactjs.org/docs/forms.html
 // https://www.apprendre-react.fr/tutorial/debutant/les-composants/
+
+
+class Form extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            clicked : false
+        }
+    }
+    render() {
+        let Form = this.props.Form;
+        return (
+            <div className="search">
+                <form>
+                    <div className="searchBar">
+                        <p>Recherche simple</p>
+                        <input type="search" className="searchBar-input" placeholder="Que recherchez-vous ?" />
+                        <button type="submit" className="searchBar-button" onClick={this.buttonClick}>Rechercher</button>
+                    </div>
+                    <div className="searchBarAdvanced">
+                        <p>Recherche avancée</p>
+                        <input type="search" className="searchBar-input" placeholder="Tapez un mot-clef" />
+                        <label className="searchBarAdvanced-label_tag">Tag
+                            <select name="tag" value={this.state.value}>
+                                <option value="{name.tag}">{name}</option>
+                                <option value="{name.tag}">{name}</option>
+                                <option value="{name.tag}">{name}</option>
+                            </select>
+                        </label>
+                        <label clasName="searchBarAdvanced-label_price">Prix
+                            <input type="radio" className="searchBarAdvanced-price" name="prix" value="gratuit" />
+                            <label for="gratuit">Gratuit</label>
+                            <input type="radio" className="searchBarAdvanced-price" name="prix" value="payant" />
+                            <label for="payant">Payant</label>
+                        </label>
+                        <fieldset className="searchBarAdvanced-label_broadcast">Diffusion internet
+                            <input type="radio" className="searchBarAdvanced-broadcast" name="diffusion" value="oui" />
+                            <label for="oui">Oui</label>
+                            <input type="radio" className="searchBarAdvanced-broadcast" name="diffusion" value="non" />
+                            <label for="non">Non</label>
+                        </fieldset>
+                        <label className="searchBarAdvanced-label_date">Date de début
+                            <input type="date" name="date" min="2019-10-01" max="2020-02-29" />
+                        </label>
+                        <label className="searchBarAdvanced-label_adress">Ville
+                            <input list="villes" id="searchBarAdvanced-label_adress-cities" name="ville" />
+                            <datalist id="villes">
+                                <option value="{address.event}" />
+                                <option value="{address.event}" />
+                                <option value="{address.event}" />
+                            </datalist>
+                        </label>
+                        <button type="submit" className="searchBar-button" onClick={ButtonClickAdvanced}>Rechercher</button>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
 
 // == Export
 export default Form;
