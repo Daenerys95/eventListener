4b517ecce557e23c8852060166dffdabeb7b72d9
Ines Mouandjo Lobe <inesmouandjolobe@gmail.com>
2019-10-22 22:08:25 +0200

    404 ok, pas de CSS dessus

--- src/components/Errors/index.js
+++ src/components/Errors/index.js
@@ -1,1 +1,12 @@
-/* page erreur 404 à faire */
+import React from 'react';
+
+import errorPage from 'src/assets/images/404.png';
+
+const Errors = () => (
+  <>
+    <img src={errorPage} alt="404" />
+    <button type="button">Page d'accueil</button>
+  </>
+);
+
+export default Errors;
